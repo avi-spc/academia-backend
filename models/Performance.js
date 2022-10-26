abfb7384a70fda18532463c65b6de3ad4c6d21e9
Avdhesh Yadavm <avdhesh7394vn@gmail.com>
2022-10-26 23:06:04 +0530

    feat: implemented student & their performance model :sparkles:

---
+++ models/Performance.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const PerformanceSchema = new mongoose.Schema(
+	{
+		studentId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'student'
+		},
+		performance: [
+			{
+				course: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'course'
+				},
+				assignments: [
+					{
+						id: {
+							type: mongoose.Schema.Types.ObjectId
+						},
+						documentId: {
+							type: mongoose.Schema.Types.ObjectId
+						},
+						marksObtained: {
+							type: Number
+						},
+						isSubmitted: {
+							type: Boolean
+						},
+						remarks: {
+							type: String
+						},
+						_id: false
+					}
+				],
+				project: {
+					id: {
+						type: mongoose.Schema.Types.ObjectId
+					},
+					title: {
+						type: String
+					},
+					synopsis: {
+						type: String
+					},
+					documentId: {
+						type: mongoose.Schema.Types.ObjectId
+					},
+					url: {
+						type: String
+					},
+					team: [
+						{
+							studentId: {
+								type: mongoose.Schema.Types.ObjectId,
+								ref: 'student'
+							}
+						}
+					],
+					isSubmitted: {
+						type: Boolean
+					},
+					remarks: {
+						type: String
+					},
+					_id: false
+				}
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
+);
+
+module.exports = Performance = mongoose.model('performance', PerformanceSchema);
---
+++ models/Student.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const StudentSchema = new mongoose.Schema(
+	{
+		instituteId: {
+			type: String,
+			required: true,
+			unique: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		name: {
+			type: String,
+			required: true
+		},
+		branch: {
+			type: String
+		},
+		batch: {
+			type: String
+		},
+		coursesEnrolled: [
+			{
+				course: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: 'course'
+				}
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
+);
+
+module.exports = Student = mongoose.model('student', StudentSchema);

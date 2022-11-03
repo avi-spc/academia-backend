4080ab44ccbf0e8586c48d2f439d0fdfec91330c
Avdhesh Yadavm <avdhesh7394vn@gmail.com>
2022-11-03 22:03:17 +0530

    sass: added font support and base variables :lipstick:

---
+++ client/src/components/course/CourseDocket.jsx
@@ -0,0 +1,9 @@
+const CourseDocket = () => {
+	return (
+		<div className="course-docket">
+			<div className="course-docket__heading">Courses</div>
+		</div>
+	);
+};
+
+export default CourseDocket;

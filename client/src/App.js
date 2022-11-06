76d1b7cc378719c65f79641d9707501ddf86c1e5
Avdhesh Yadavm <avdhesh7394vn@gmail.com>
2022-11-06 19:38:34 +0530

    feat: added redux store :sparkles:

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,11 @@
-import Chore from './components/create/Chore';
-import CourseCard from './components/course/CourseCard';
-import CourseDocket from './components/course/CourseDocket';
-import IndividualCourse from './components/course/IndividualCourse';
-import Course from './components/create/Course';
-import Navbar from './components/header/Navabr';
-import Announcement from './components/create/Announcement';
-import Discussion from './components/create/Discussion';
-import SignUp from './components/auth/SignUp';
-import SignIn from './components/auth/SignIn';
+import { Provider } from 'react-redux';
+
+import store from './reduxStore/store';
 
 const App = () => {
 	return (
 		<div className="App">
-			<SignIn />
+			{/* <Provider store={store}></Provider> */}
 		</div>
 	);
 };
